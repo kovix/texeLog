63ecea8a5af915fca9f073fc59ddc71d9f151668
Daniel Kovacs <info@kovix.info>
2022-10-15 10:16:26 +0200

    Implement base logging

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const path = require('path');
+const path = require('path')
 const mqtt = require('mqtt')
 const bunyan = require('bunyan')
 const RotatingFileStream = require('bunyan-rotating-file-stream')
@@ -34,6 +34,77 @@ const bunyanOpts = {
 }
 const log = bunyan.createLogger(bunyanOpts)
 
+const parseZoneStatus = (status) => {
+  const statuses = {
+    0: 'Secure',
+    1: 'active',
+  }
+  return statuses[status] || status;
+}
 
-//const clientId = `texeLogger_${Math.random().toString(16).slice(3)}`
+const storeAreaMessage = (payload) => {
+  try {
+    const message = JSON.parse(payload);
+    log.info(`AREA event, ID: ${message.id}, Number: ${message.number}, Name: ${message.name}, Status: ${message.status}`)
+  } catch(error) {
+    log.error('Unable to parse AREA MQTT payload.', payload, error)
+  }
+}
 
+
+const storeZoneMessage = (payload) => {
+  try {
+    const message = JSON.parse(payload);
+    const areas = message.areas.join(', ')
+    log.info(`ZONE event, Number: ${message.number}, Name: ${message.name} (${message.type}), Areas: ${areas},  Status: ${parseZoneStatus(message.status)}`)
+  } catch(error) {
+    log.error('Unable to parse ZONE MQTT payload.', payload, error)
+  }
+}
+
+//check mandatory params
+if (!process.env.MQTT_PROTOCOL ||
+  !process.env.MQTT_HOST ||
+  !process.env.MQTT_PORT ||
+  !process.env.MQTT_TOPIC_PREFIX) {
+  log.error('Mandatory MQTT params missing. Please set at least MQTT_PROTOCOL MQTT_HOST MQTT_PORT in .env')
+  process.exit()
+}
+
+const topics = [
+  `${process.env.MQTT_TOPIC_PREFIX}/zone/#`,
+  `${process.env.MQTT_TOPIC_PREFIX}/area/#`,
+]
+const clientId = `texeLogger_${Math.random().toString(16).slice(3)}`
+const connectUrl = `${process.env.MQTT_PROTOCOL}://${process.env.MQTT_HOST}:${process.env.MQTT_PORT}`
+const mqttOpts = {
+  clientId,
+  clean: true,
+  connectTimeout: 4000,
+  reconnectPeriod: 1000,
+}
+
+if (process.env.MQTT_USER && process.env.MQTT_PASS) {
+  mqttOpts.username = process.env.MQTT_USER
+  mqttOpts.password = process.env.MQTT_PASS
+}
+
+const mqttClient = mqtt.connect(connectUrl, mqttOpts)
+
+mqttClient.on('connect', () => {
+  log.info(`Connected to ${connectUrl} with client id ${clientId}`)
+  mqttClient.subscribe(topics, () => {
+    log.info('Subscribe to topics')
+  })
+})
+
+mqttClient.on('message', (topic, payload) => {
+  if (topic.startsWith(`${process.env.MQTT_TOPIC_PREFIX}/zone`)) {
+    storeZoneMessage(payload.toString())
+    return
+  }
+  if (topic.startsWith(`${process.env.MQTT_TOPIC_PREFIX}/area`)) {
+    storeAreaMessage(payload.toString())
+    return
+  }
+})
